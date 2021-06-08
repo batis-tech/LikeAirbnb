f74a5185138b4b3173209d802287651c9bdc13b2
hasan <hasanbatis440@gmail.com>
2021-06-08 19:34:58 +0300

    adding properties and adjusting devise,editing front-end

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+
+  get 'dashboard/index', to:"dashboard#index", as: :dashboard
+  get 'dashboard/properties'
+  get 'dashboard/reports'
+
+  resources :properties
+  devise_for :accounts
+  root 'public#main'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
